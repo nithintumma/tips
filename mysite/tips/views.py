9fa2c3852211f35bf1a6cc83a319d33a31af2e21
Nithin Tumma <ntumma@college.harvard.edu>
2013-04-16 00:50:57 -0400

    Just for fun

--- mysite/tips/views.py
+++ mysite/tips/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return H
+    latest_review_list = Review.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('tips/index.html')
+    context = Context({
+            'latest_review_list': latest_review_list,
+    })
+    return HttpResponse(template.render(context))
