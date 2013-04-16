774e518ff524ad1ebc53bb05fdfe1e9e64d2725d
Nithin Tumma <ntumma@college.harvard.edu>
2013-04-15 22:04:33 -0400

    second commit

--- mysite/tips/models.py
+++ mysite/tips/models.py
@@ -17,5 +17,23 @@ class Service (models.Model):
 class Reviews(models.Model):
     stylist = models.CharField(max_length = 200)
     salon = models.CharField(max_length = 200)
-    service = models.CharField(max_lengt)
-    
+    service = models.CharField(max_length = 200)
+   
+    # ratings, out of 5 stars
+    salon_rating = model.IntegerField()
+    service_rating = model.IntegerField()
+    stylist_rating = model.IntegerField()
+    atmosphere_rating = model.IntegerField()
+    service_rating =model.IntegerField()
+    wait_rating =model.IntegerField()
+    personality_rating =model.IntegerField()
+    talking_rating =model.IntegerField()
+    expertise_rating =model.IntegerField()
+    price_rating =model.IntegerField()
+    quality_rating =model.IntegerField()
+
+    # text based feedback
+    salon_feedback = model.TextField()
+    stylist_feedback = model.TextField()
+    service_feedback = model.TextField()
+
