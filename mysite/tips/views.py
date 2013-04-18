8dc2a418f736935e6263042491f32736eedceeca
Neel Patel <npatel@college.harvard.edu>
2013-04-17 22:13:35 -0400

    Final update with working functionality

--- mysite/tips/views.py
+++ mysite/tips/views.py
@@ -19,7 +19,27 @@ def submit (request):
 	if request.method == 'POST': # If the form has been submitted...
 		form = ReviewForm(request.POST) # A form bound to the POST data
         form.save() 
-        return HttpResponseRedirect('/')    
+        return HttpResponseRedirect('/')  
+
+def search(request):
+	if 'q' in request.GET and request.GET['q']: # If the form has been submitted...
+		q = request.GET['q']
+		results = Reviews.objects.filter(salon__icontains=q)
+		labels = {  'atmosphere_rating': ['unpleasant', 'pleasant'],
+					'wait_rating': ['Very fast', 'Average', 'Slow'],
+					'personality_rating': ['unpleasant', 'pleasant'],
+					'talking_rating': ['Too quiet', 'Average', 'Too talkative'],
+					'expertise_rating': ['Unexperienced', 'Average', 'Very experienced'],
+					'price_rating': ['Cheap', 'Average', 'Expensive'],
+					'quality_rating': ['Terrible', 'Decent', 'Excellent']
+		}				
+		return render(request, 'tips/search.html', 
+			{'results': results, 'query': q, 'labels': labels})
+	elif request.method == 'GET':
+		return render(request, 'tips/search.html', ({}))
+
+from django import template
+register = template.Library()
 
 # type is from the set (salon, service, )
 #def show (request, salon_name, type):
