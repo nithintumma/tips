a19d8cdfea7c414a7f65c27d5d9cc94263a63788
Neel Patel <npatel@college.harvard.edu>
2013-04-19 10:34:53 -0400

    wsgi fix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mysite.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/mysite/html/django_templates" or "C:/www/django/templates".
